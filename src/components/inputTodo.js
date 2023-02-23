05b3a3dc8e55c87bab854566f17960b75f7f2a17
Wajid Khan <wajid.developer1@gmail.com>
2023-02-23 21:32:37 +0530

    All files

---
+++ src/components/inputTodo.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+
+const InputTodo = () => {
+    return (
+    <Fragment>
+        <h1 className="text-center mt-3">Create Todo</h1>
+        <form method="post">
+            
+        </form>
+    </Fragment>
+    )
+};
+
+export default InputTodo;
